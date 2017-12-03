5c6478a50a5544d195608b8e681ebd5ad673b445
Cesar Laredo <cesar.f.laredo@gmail.com>
2017-12-03 16:51:19 -0700

    Memory functionality

---
+++ arduino-code/SimpleTimer/SimpleTimer.cpp
@@ -0,0 +1,490 @@
+ /*
+ * SimpleTimer.cpp
+ *
+ * SimpleTimer - A timer library for Arduino.
+ * Author: mromani@ottotecnica.com
+ * Copyright (c) 2010 OTTOTECNICA Italy
+ *
+ * This library is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software
+ * Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ *
+ * This library is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser
+ * General Public License along with this library; if not,
+ * write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+
+#include "SimpleTimer.h"
+
+
+// Select time function:
+//static inline unsigned long elapsed() { return micros(); }
+static inline unsigned long elapsed() { return millis(); }
+
+
+SimpleTimer::SimpleTimer() {
+    unsigned long current_millis = elapsed();
+
+    for (int i = 0; i < MAX_TIMERS; i++) {
+        enabled[i] = false;
+        callbacks[i] = 0;                   // if the callback pointer is zero, the slot is free, i.e. doesn't "contain" any timer
+        prev_millis[i] = current_millis;
+        numRuns[i] = 0;
+    }
+
+    numTimers = 0;
+}
+
+
+void SimpleTimer::run() {
+    int i;
+    unsigned long current_millis;
+
+    // get current time
+    current_millis = elapsed();
+
+    for (i = 0; i < MAX_TIMERS; i++) {
+
+        toBeCalled[i] = DEFCALL_DONTRUN;
+
+        // no callback == no timer, i.e. jump over empty slots
+        if (callbacks[i]) {
+
+            // is it time to process this timer ?
+            // see http://arduino.cc/forum/index.php/topic,124048.msg932592.html#msg932592
+
+            if (current_millis - prev_millis[i] >= delays[i]) {
+
+                // update time
+                //prev_millis[i] = current_millis;
+                prev_millis[i] += delays[i];
+
+                // check if the timer callback has to be executed
+                if (enabled[i]) {
+
+                    // "run forever" timers must always be executed
+                    if (maxNumRuns[i] == RUN_FOREVER) {
+                        toBeCalled[i] = DEFCALL_RUNONLY;
+                    }
+                    // other timers get executed the specified number of times
+                    else if (numRuns[i] < maxNumRuns[i]) {
+                        toBeCalled[i] = DEFCALL_RUNONLY;
+                        numRuns[i]++;
+
+                        // after the last run, delete the timer
+                        if (numRuns[i] >= maxNumRuns[i]) {
+                            toBeCalled[i] = DEFCALL_RUNANDDEL;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    for (i = 0; i < MAX_TIMERS; i++) {
+        switch(toBeCalled[i]) {
+            case DEFCALL_DONTRUN:
+                break;
+
+            case DEFCALL_RUNONLY:
+                (*callbacks[i])();
+                break;
+
+            case DEFCALL_RUNANDDEL:
+                (*callbacks[i])();
+                deleteTimer(i);
+                break;
+        }
+    }
+}
+
+
+// find the first available slot
+// return -1 if none found
+int SimpleTimer::findFirstFreeSlot() {
+    int i;
+
+    // all slots are used
+    if (numTimers >= MAX_TIMERS) {
+        return -1;
+    }
+
+    // return the first slot with no callback (i.e. free)
+    for (i = 0; i < MAX_TIMERS; i++) {
+        if (callbacks[i] == 0) {
+            return i;
+        }
+    }
+
+    // no free slots found
+    return -1;
+}
+
+
+int SimpleTimer::setTimer(long d, timer_callback f, int n) {
+    int freeTimer;
+
+    freeTimer = findFirstFreeSlot();
+    if (freeTimer < 0) {
+        return -1;
+    }
+
+    if (f == NULL) {
+        return -1;
+    }
+
+    delays[freeTimer] = d;
+    callbacks[freeTimer] = f;
+    maxNumRuns[freeTimer] = n;
+    enabled[freeTimer] = true;
+    prev_millis[freeTimer] = elapsed();
+
+    numTimers++;
+
+    return freeTimer;
+}
+
+
+int SimpleTimer::setInterval(long d, timer_callback f) {
+    return setTimer(d, f, RUN_FOREVER);
+}
+
+
+int SimpleTimer::setTimeout(long d, timer_callback f) {
+    return setTimer(d, f, RUN_ONCE);
+}
+
+
+void SimpleTimer::deleteTimer(int timerId) {
+    if (timerId >= MAX_TIMERS) {
+        return;
+    }
+
+    // nothing to delete if no timers are in use
+    if (numTimers == 0) {
+        return;
+    }
+
+    // don't decrease the number of timers if the
+    // specified slot is already empty
+    if (callbacks[timerId] != NULL) {
+        callbacks[timerId] = 0;
+        enabled[timerId] = false;
+        toBeCalled[timerId] = DEFCALL_DONTRUN;
+        delays[timerId] = 0;
+        numRuns[timerId] = 0;
+
+        // update number of timers
+        numTimers--;
+    }
+}
+
+
+// function contributed by code@rowansimms.com
+void SimpleTimer::restartTimer(int numTimer) {
+    if (numTimer >= MAX_TIMERS) {
+        return;
+    }
+
+    prev_millis[numTimer] = elapsed();
+}
+
+
+boolean SimpleTimer::isEnabled(int numTimer) {
+    if (numTimer >= MAX_TIMERS) {
+        return false;
+    }
+
+    return enabled[numTimer];
+}
+
+
+void SimpleTimer::enable(int numTimer) {
+    if (numTimer >= MAX_TIMERS) {
+        return;
+    }
+
+    enabled[numTimer] = true;
+}
+
+
+void SimpleTimer::disable(int numTimer) {
+    if (numTimer >= MAX_TIMERS) {
+        return;
+    }
+
+    enabled[numTimer] = false;
+}
+
+
+void SimpleTimer::toggle(int numTimer) {
+    if (numTimer >= MAX_TIMERS) {
+        return;
+    }
+
+    enabled[numTimer] = !enabled[numTimer];
+}
+
+
+int SimpleTimer::getNumTimers() {
+    return numTimers;
+}/*
+ * SimpleTimer.cpp
+ *
+ * SimpleTimer - A timer library for Arduino.
+ * Author: mromani@ottotecnica.com
+ * Copyright (c) 2010 OTTOTECNICA Italy
+ *
+ * Callback function parameters added & compiler warnings
+ * removed by Bill Knight <billk@rosw.com> 20March2017
+ *
+ * This library is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software
+ * Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ *
+ * This library is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser
+ * General Public License along with this library; if not,
+ * write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+
+#include "SimpleTimer.h"
+
+
+// Select time function:
+//static inline unsigned long elapsed() { return micros(); }
+static inline unsigned long elapsed() { return millis(); }
+
+
+SimpleTimer::SimpleTimer() {
+    unsigned long current_millis = elapsed();
+
+    for (int i = 0; i < MAX_TIMERS; i++) {
+        memset(&timer[i], 0, sizeof (timer_t));
+        timer[i].prev_millis = current_millis;
+    }
+
+    numTimers = 0;
+}
+
+
+void SimpleTimer::run() {
+    int i;
+    unsigned long current_millis;
+
+    // get current time
+    current_millis = elapsed();
+
+    for (i = 0; i < MAX_TIMERS; i++) {
+
+        timer[i].toBeCalled = DEFCALL_DONTRUN;
+
+        // no callback == no timer, i.e. jump over empty slots
+        if (timer[i].callback != NULL) {
+
+            // is it time to process this timer ?
+            // see http://arduino.cc/forum/index.php/topic,124048.msg932592.html#msg932592
+
+            if ((current_millis - timer[i].prev_millis) >= timer[i].delay) {
+
+                // update time
+                timer[i].prev_millis += timer[i].delay;
+
+                // check if the timer callback has to be executed
+                if (timer[i].enabled) {
+
+                    // "run forever" timers must always be executed
+                    if (timer[i].maxNumRuns == RUN_FOREVER) {
+                        timer[i].toBeCalled = DEFCALL_RUNONLY;
+                    }
+                    // other timers get executed the specified number of times
+                    else if (timer[i].numRuns < timer[i].maxNumRuns) {
+                        timer[i].toBeCalled = DEFCALL_RUNONLY;
+                        timer[i].numRuns++;
+
+                        // after the last run, delete the timer
+                        if (timer[i].numRuns >= timer[i].maxNumRuns) {
+                            timer[i].toBeCalled = DEFCALL_RUNANDDEL;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    for (i = 0; i < MAX_TIMERS; i++) {
+        if (timer[i].toBeCalled == DEFCALL_DONTRUN)
+            continue;
+
+        if (timer[i].hasParam)
+            (*(timer_callback_p)timer[i].callback)(timer[i].param);
+        else
+            (*(timer_callback)timer[i].callback)();
+
+        if (timer[i].toBeCalled == DEFCALL_RUNANDDEL)
+            deleteTimer(i);
+    }
+}
+
+
+// find the first available slot
+// return -1 if none found
+int SimpleTimer::findFirstFreeSlot() {
+    // all slots are used
+    if (numTimers >= MAX_TIMERS) {
+        return -1;
+    }
+
+    // return the first slot with no callback (i.e. free)
+    for (int i = 0; i < MAX_TIMERS; i++) {
+        if (timer[i].callback == NULL) {
+            return i;
+        }
+    }
+
+    // no free slots found
+    return -1;
+}
+
+
+int SimpleTimer::setupTimer(unsigned long d, void* f, void* p, boolean h, unsigned n) {
+    int freeTimer;
+
+    freeTimer = findFirstFreeSlot();
+    if (freeTimer < 0) {
+        return -1;
+    }
+
+    if (f == NULL) {
+        return -1;
+    }
+
+    timer[freeTimer].delay = d;
+    timer[freeTimer].callback = f;
+    timer[freeTimer].param = p;
+    timer[freeTimer].hasParam = h;
+    timer[freeTimer].maxNumRuns = n;
+    timer[freeTimer].enabled = true;
+    timer[freeTimer].prev_millis = elapsed();
+
+    numTimers++;
+
+    return freeTimer;
+}
+
+
+int SimpleTimer::setTimer(unsigned long d, timer_callback f, unsigned n) {
+  return setupTimer(d, (void *)f, NULL, false, n);
+}
+
+int SimpleTimer::setTimer(unsigned long d, timer_callback_p f, void* p, unsigned n) {
+  return setupTimer(d, (void *)f, p, true, n);
+}
+
+int SimpleTimer::setInterval(unsigned long d, timer_callback f) {
+    return setupTimer(d, (void *)f, NULL, false, RUN_FOREVER);
+}
+
+int SimpleTimer::setInterval(unsigned long d, timer_callback_p f, void* p) {
+  return setupTimer(d, (void *)f, p, true, RUN_FOREVER);
+}
+
+int SimpleTimer::setTimeout(unsigned long d, timer_callback f) {
+    return setupTimer(d, (void *)f, NULL, false, RUN_ONCE);
+}
+
+int SimpleTimer::setTimeout(unsigned long d, timer_callback_p f, void* p) {
+  return setupTimer(d, (void *)f, p, true, RUN_ONCE);
+}
+
+
+void SimpleTimer::deleteTimer(unsigned timerId) {
+    if (timerId >= MAX_TIMERS) {
+        return;
+    }
+
+    // nothing to delete if no timers are in use
+    if (numTimers == 0) {
+        return;
+    }
+
+    // don't decrease the number of timers if the
+    // specified slot is already empty
+    if (timer[timerId].callback != NULL) {
+        memset(&timer[timerId], 0, sizeof (timer_t));
+        timer[timerId].prev_millis = elapsed();
+
+        // update number of timers
+        numTimers--;
+    }
+}
+
+
+// function contributed by code@rowansimms.com
+void SimpleTimer::restartTimer(unsigned numTimer) {
+    if (numTimer >= MAX_TIMERS) {
+        return;
+    }
+
+    timer[numTimer].prev_millis = elapsed();
+}
+
+
+boolean SimpleTimer::isEnabled(unsigned numTimer) {
+    if (numTimer >= MAX_TIMERS) {
+        return false;
+    }
+
+    return timer[numTimer].enabled;
+}
+
+
+void SimpleTimer::enable(unsigned numTimer) {
+    if (numTimer >= MAX_TIMERS) {
+        return;
+    }
+
+    timer[numTimer].enabled = true;
+}
+
+
+void SimpleTimer::disable(unsigned numTimer) {
+    if (numTimer >= MAX_TIMERS) {
+        return;
+    }
+
+    timer[numTimer].enabled = false;
+}
+
+
+void SimpleTimer::toggle(unsigned numTimer) {
+    if (numTimer >= MAX_TIMERS) {
+        return;
+    }
+
+    timer[numTimer].enabled = !timer[numTimer].enabled;
+}
+
+
+unsigned SimpleTimer::getNumTimers() {
+    return numTimers;
+}
--- arduino-code/arduino-code.ino
+++ arduino-code/arduino-code.ino
@@ -1,9 +1,19 @@
 #include <SoftwareSerial.h>
-//#include <EEPROM.h>
 #include <EEPROMex.h>
+#include <SimpleTimer.h>
 
-SoftwareSerial Bluetooth(4,2); // RX, TX
-String opcion;
+//PORTS
+const int flowSensor = 2;
+const int valveSensor = 3;
+const int bluetoothRX = 4;
+const int bluetoothTX = 5;
+const int tlcA4 = 18;
+const int tlcA5 = 19;
+
+SoftwareSerial Bluetooth(bluetoothRX, bluetoothTX); // RX, TX
+
+// the timer object
+SimpleTimer timer;
 
 //constantes para especificar categoria de Comandos (Command Type)
 const String SYNC = "SYNC";
@@ -12,26 +22,26 @@ const String VOLUME = "VOLUME";
 const String TIME = "TIME";
 const String EXEC = "EXEC";
 
+//Value Constants
+const byte MODE_TIME = 0;
+const byte MODE_VOLUME = 1;
+
 //Memory values for storing variable addresses
 const int MODE_MEMORY_ADDRESS = EEPROM.getAddress(sizeof(byte)); //Mode Options are 0 or 1 (By Liters or By Time)
-const int TIME_OF_DAY_RUN_HOUR_MEMORY_ADDRESS = MODE_MEMORY_ADDRESS + EEPROM.getAddress(sizeof(int)); // int (0 - 23
+const int TIME_OF_DAY_RUN_HOUR_MEMORY_ADDRESS = MODE_MEMORY_ADDRESS + EEPROM.getAddress(sizeof(int)); // int (0 - 23)
 const int TIME_OF_DAY_RUN_MINUTE_MEMORY_ADDRESS = TIME_OF_DAY_RUN_HOUR_MEMORY_ADDRESS + EEPROM.getAddress(sizeof(int)); // int (0 - 60)
 const int MILLITERS_MEMORY_ADDRESS = TIME_OF_DAY_RUN_MINUTE_MEMORY_ADDRESS + EEPROM.getAddress(sizeof(long)); // long (milliters to store)
 const int RUN_TIME_MEMORY_ADDRESS_MINUTES = MILLITERS_MEMORY_ADDRESS + EEPROM.getAddress(sizeof(int)); // int (number of minutes)
 
 int cantidad=0; //Volumen de agua requerida por el usuario en litros.
-int valor=0; //Valor que viene "tecla" usando una conversion numerica: atoi().
+int closeValveInterval; //Interval Container - Used to Cancel Timeout Callback
 
 //Variables utilizadas para la lectura del sensor de flujo.
 volatile int NumPulsos; //variable para la cantidad de pulsos recibidos. Como se usa dentro de una interrupcion debe ser volatile
-int PinSensor = 5;    //Sensor conectado en el pin 2
 float factor_conversion=7.11; //para convertir de frecuencia a caudal
 float volumen=0;
 long dt=0; //variación de tiempo por cada bucle
 long t0=0; //millis() del bucle anterior
- 
-//Valvula solonoide.
-int solenoidPin = 3; // Este es el pin de salida para la valvula solonoide
 
 //START MEMORY GETTERS AND SETTERS
 int setMemoryValue(int address, int value) {
@@ -47,54 +57,19 @@ int setMemoryValue(int address, long value) {
 }
 
 int readMemoryValue (int address) {
-  EEPROM.readInt(address);
+  return EEPROM.readInt(address);
 }
 
 byte readMemoryValueByte (int address) {
-    EEPROM.readByte(address);
+  return EEPROM.readByte(address);
 }
 
 long readMemoryValueLong (int address) {
-    EEPROM.readByte(address);
+  return EEPROM.readByte(address);
 }
 //END MEMORY GETTERS AND SETTERS
 
-void setup()
-{
-  Serial.begin(9600); 
-  Bluetooth.begin(9600);
-  pinMode(PinSensor, INPUT); 
-  attachInterrupt(0,ContarPulsos,RISING); //(Interrupción 0(Pin2),función,Flanco de subida)
-  t0=millis();
-  
-  pinMode (solenoidPin, OUTPUT); // Establece el pin de la valvula como salida 
-}
-
-void loop() {
-  if (Bluetooth.available()) {
-      String opcion = Bluetooth.readString();
-      Serial.println("Opcion: "+opcion);
-      parseSerialBluetooth(opcion);
-  }
-}
-
-
-void ContarPulsos () //Función que se ejecuta en interrupción  
-{ 
-  NumPulsos++;  //incrementamos la variable de pulsos
-} 
-
-int ObtenerFrecuecia() //Función para obtener frecuencia de los pulsos
-{
-  int frecuencia;
-  NumPulsos = 0;   //Ponemos a 0 el número de pulsos
-  interrupts();    //Habilitamos las interrupciones
-  delay(1000);   //muestra de 1 segundo
-  noInterrupts(); //Deshabilitamos  las interrupciones
-  frecuencia=NumPulsos; //Hz(pulsos por segundo)
-  return frecuencia;
-}
-
+//START HELPER FUNCTIONS
 String GetLine()
  {   String S = "" ;
      if (Serial.available())
@@ -123,29 +98,119 @@ String getValueHelper(String data, char separator, int index)
     }
     return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
+//END HELPER FUNCTIONS
 
-bool setSystemMode(String mode) {
-  if (mode == TIME) {
-     // TODO: set mode value to "VOLUME";
-     Serial.println("Setting Mode to VOLUME");
+class ValveState {
+  public:
+    byte valveMode;
+    int timeToRunHour;
+    int timeToRunMinute;
+    long mililitersToRun;
+    int minutesToRun;
+    ValveState();
+    void getSavedState () {
+      valveMode = readMemoryValueByte(MODE_MEMORY_ADDRESS);
+      timeToRunHour = readMemoryValue(TIME_OF_DAY_RUN_HOUR_MEMORY_ADDRESS);
+      timeToRunMinute = readMemoryValue(TIME_OF_DAY_RUN_MINUTE_MEMORY_ADDRESS);
+      mililitersToRun = readMemoryValueLong(MILLITERS_MEMORY_ADDRESS);
+      minutesToRun = readMemoryValue(RUN_TIME_MEMORY_ADDRESS_MINUTES);
+    }
+    void setValveMode (byte mode) {
+      valveMode = mode;
+      setMemoryValue(MODE_MEMORY_ADDRESS, valveMode);
+    }
+    void setTimeToRun (int hour, int minute) {
+      timeToRunHour = hour;
+      timeToRunMinute = minute;
+      setMemoryValue(TIME_OF_DAY_RUN_HOUR_MEMORY_ADDRESS, timeToRunHour);
+      setMemoryValue(TIME_OF_DAY_RUN_MINUTE_MEMORY_ADDRESS, timeToRunMinute);
+    }
+    void setMililiters (long mililiters) {
+      mililitersToRun = mililiters;
+      setMemoryValue(MILLITERS_MEMORY_ADDRESS, mililitersToRun);
+    }
+    void setMinutesToRun (int minutes) {
+      minutesToRun = minutes;
+      setMemoryValue(RUN_TIME_MEMORY_ADDRESS_MINUTES, minutesToRun);
+    }
+    void saveCurrentState () {
+      setMemoryValue(MODE_MEMORY_ADDRESS, valveMode);
+      setMemoryValue(TIME_OF_DAY_RUN_HOUR_MEMORY_ADDRESS, timeToRunHour);
+      setMemoryValue(TIME_OF_DAY_RUN_MINUTE_MEMORY_ADDRESS, timeToRunMinute);
+      setMemoryValue(MILLITERS_MEMORY_ADDRESS, mililitersToRun);
+      setMemoryValue(RUN_TIME_MEMORY_ADDRESS_MINUTES, minutesToRun);
+    }
+};
+
+ValveState::ValveState(void) {
+  getSavedState();
+}
+
+ValveState valve;
+
+void setup()
+{
+  Serial.begin(9600); 
+  Bluetooth.begin(9600);
+  pinMode(flowSensor, INPUT); 
+  attachInterrupt(0,ContarPulsos,RISING); //(Interrupción 0(Pin2),función,Flanco de subida)
+  pinMode(valveSensor, OUTPUT); // Establece el pin de la valvula como salida
+  t0= millis();
+  Serial.println(MODE_MEMORY_ADDRESS);
+  Serial.println(TIME_OF_DAY_RUN_HOUR_MEMORY_ADDRESS);
+  Serial.println(TIME_OF_DAY_RUN_MINUTE_MEMORY_ADDRESS);
+  Serial.println(MILLITERS_MEMORY_ADDRESS);
+  Serial.println(RUN_TIME_MEMORY_ADDRESS_MINUTES);
+  valve = ValveState();
+}
+
+void loop() {
+  if (Bluetooth.available()) {
+      String opcion = Bluetooth.readString();
+      Serial.println("Bluetooth Option: "+opcion);
+      stopValve();
+      parseSerialBluetooth(opcion);
   }
-    else if (mode == VOLUME) {
-     // TODO: set mode value to "TIME";
-     Serial.println("Setting Mode to VOLUME");
-  }
-  else {
-    //Empty Block
-  }
-  return true;  
+}
+
+void ContarPulsos () //Función que se ejecuta en interrupción  
+{ 
+  NumPulsos++;  //incrementamos la variable de pulsos
+} 
+
+int ObtenerFrecuecia() //Función para obtener frecuencia de los pulsos
+{
+  int frecuencia;
+  NumPulsos = 0;   //Ponemos a 0 el número de pulsos
+  interrupts();    //Habilitamos las interrupciones
+  delay(1000);   //muestra de 1 segundo
+  noInterrupts(); //Deshabilitamos  las interrupciones
+  frecuencia=NumPulsos; //Hz(pulsos por segundo)
+  return frecuencia;
 }
 
 bool parseSerialBluetooth(String command) {
   String commandInit = getValueHelper(command, ':', 0);
-  if (commandInit == "YS") {
+  if (commandInit == "LS") {
     String commandType = getValueHelper(command,':',1);
     if (commandType == MODE) {
       String systemMode = getValueHelper(command,':', 2);
       setSystemMode(systemMode);
+    }
+    else if (commandType == VOLUME) {
+      String volume = getValueHelper(command,':', 2);
+      if (volume) {
+        setVolume(volume);
+      }
+    }
+    else if (commandType == TIME) {
+      String fullTimeString = getValueHelper(command,':', 2);
+      if (fullTimeString) {
+        setTime(fullTimeString);
+      }
+    }
+    else if (commandType == EXEC) {
+      //TODO: RUN OPEN VALVE
     }
     else {
       Serial.println("Other Value: " + command);
@@ -155,7 +220,63 @@ bool parseSerialBluetooth(String command) {
   return false;
 }
 
+bool setTime(String fullTimeString) {
+  String hourString = getValueHelper(fullTimeString,',', 0);
+  String minuteString = getValueHelper(fullTimeString,',', 1);
+  if (hourString && minuteString) {
+    int hour = hourString.toInt();
+    int minute = minuteString.toInt();
+    if (hour >= 0 && hour < 24 && minute >= 0 && minute < 60) {
+      valve.setTimeToRun(hour, minute);
+      return true;
+    }
+  }
+  return false;
+}
+
+bool setVolume(String volume) {
+  long vol = volume.toInt();
+  if (vol) {
+    valve.setMililiters(vol);
+    return true;
+  }
+}
+
+bool setSystemMode(String mode) {
+  if (mode == VOLUME) {
+     // TODO: set mode value to "VOLUME";
+     Serial.println("Setting Mode to VOLUME");
+     valve.setValveMode(MODE_VOLUME);
+  }
+  else if (mode == TIME) {
+     // TODO: set mode value to "TIME";
+     Serial.println("Setting Mode to TIME");
+     valve.setValveMode(MODE_TIME);
+  }
+  return true;  
+}
+
+void stopValve () {
+  digitalWrite(valveSensor, LOW);
+}
+
 void openValve () {
+  if (valve.valveMode == MODE_TIME) {
+    openValveTime();
+  }
+  else if (valve.valveMode == MODE_VOLUME) {
+    openValveVolume();
+  }
+}
+
+void openValveTime () {
+  digitalWrite(valveSensor, HIGH);
+  long timeToRun = valve.minutesToRun * 60 * 1000;
+  //After valve has run it's course of time close the valve
+  closeValveInterval = timer.setTimeout(timeToRun, stopValve);
+}
+
+void openValveVolume () {
   while(volumen<=cantidad)
   {
    float frecuencia=ObtenerFrecuecia(); //obtenemos la frecuencia de los pulsos en Hz
